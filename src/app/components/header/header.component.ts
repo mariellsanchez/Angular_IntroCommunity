6d9be6e88ed17f5c105950585d3c6446d95bfe46
Mariell Sanchez <mariellsanchez@estudiantec.cr>
2021-06-28 13:12:36 -0600

    Basic components
    Header, Footer, Body

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  template: `<h1>HOLA MUNDO</h1>`,
+  styleUrls: ['./header.component.css']
+})
+
+export class HeaderComponent {
+
+  constructor() { }
+
+}
+
+// export class HeaderComponent implements OnInit {
+
+//   constructor() { }
+
+//   ngOnInit(): void {
+//   }
+
+// }
